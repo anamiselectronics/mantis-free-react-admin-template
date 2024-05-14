43ddb9830020afe8c40aab0551651b232a6f9001
GambroTech <anamiselectronics@gmail.com>
2024-05-14 21:25:22 +0330

    fit the image with background outlinedInput.03.02.25

--- src/pages/reportPages/posibilitySection2.js
+++ src/pages/reportPages/posibilitySection2.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import InputAdornment from '@mui/material/InputAdornment';
 import { FormControl, FormHelperText, OutlinedInput, TextField, Divider } from '@mui/material';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+// import AccountCircle from '@mui/icons-material/AccountCircle';
 
 import MaskedInput from 'react-text-mask';
 import plate from '../../assets/images/pages/plate.png';
@@ -12,25 +12,27 @@ export default function PosibilitySection2() {
     <Box sx={{ '& > :not(style)': { m: 1 } }}>
       <MaskedInput
         guide={true}
-        mask={[' ', /\d/, ' ', /\d/, ' ', ' ', /[ا-ی]/, ' ', ' ', /\d/, ' ', /\d/, ' ', /\d/, ' ']}
+        mask={[' ', ' ', ' ', /\d/, ' ', /\d/, ' ', ' ', /[ا-ی]/, ' ', ' ', /\d/, ' ', /\d/, ' ', /\d/, ' ']}
         style={{
           fontSize: '1.5rem',
           backgroundImage: `url(${plate})`,
           objectFit: 'fill',
-          height: '30%'
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          height: '9vh'
         }}
-        height={40}
         showMask
         autoFocus
       />
       <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
         <OutlinedInput
           id="outlined-adornment-weight"
-          endAdornment={
-            <InputAdornment position="end">
-              <AccountCircle />
-            </InputAdornment>
-          }
+          // endAdornment={
+          //   <InputAdornment position="end">
+          //     <AccountCircle />
+          //   </InputAdornment>
+          // }
           startAdornment={
             <InputAdornment position="start" sx={{ marginRight: 0 }}>
               <TextField
@@ -40,7 +42,7 @@ export default function PosibilitySection2() {
                 variant="standard"
                 size="small"
                 inputProps={{ maxLength: 2 }}
-                sx={{ alignItem: 'left', width: '40%' }}
+                sx={{ alignItem: 'center', width: '40%' }}
                 inputComponent={MaskedInput}
               />
               <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
@@ -51,7 +53,7 @@ export default function PosibilitySection2() {
             'aria-label': 'weight',
             maxLength: 6
           }}
-          sx={{backgroundImage: `url(${plate})`,}}
+          sx={{ backgroundImage: `url(${plate})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}
         />
 
         <FormHelperText id="outlined-weight-helper-text">پلاک ملی</FormHelperText>
