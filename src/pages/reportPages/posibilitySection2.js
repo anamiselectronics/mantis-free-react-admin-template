dac747991bb8828199469efa7f14fe19f951b916
GambroTech <anamiselectronics@gmail.com>
2024-05-14 16:09:19 +0330

    create mask for enter car's number 03.02.25

--- src/pages/reportPages/posibilitySection2.js
+++ src/pages/reportPages/posibilitySection2.js
@@ -1,26 +1,52 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
+import Box from '@mui/material/Box';
+import InputAdornment from '@mui/material/InputAdornment';
+import { FormControl, FormHelperText, OutlinedInput, TextField, Divider } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-export default function PosiibilitySection2() {
+import MaskedInput from 'react-text-mask';
+
+export default function PosibilitySection2() {
   return (
-    <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
-      <IconButton sx={{ p: '10px' }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search Google Maps" inputProps={{ 'aria-label': 'search google maps' }} />
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
+    <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-weight"
+          endAdornment={
+            <InputAdornment position="end">
+              <AccountCircle />
+            </InputAdornment>
+          }
+          startAdornment={
+            <InputAdornment position="start" sx={{ marginRight: 0 }}>
+              <TextField
+                label="ایران"
+                id="filled-hidden-label-small"
+                defaultValue="84"
+                variant="standard"
+                size="small"
+                inputProps={{ maxLength: 2 }}
+                sx={{ alignItem: 'left', width: '40%' }}
+              />
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            </InputAdornment>
+          }
+          aria-describedby="outlined-weight-helper-text"
+          inputProps={{
+            'aria-label': 'weight',
+            maxLength: 6
+          }}
+        />
+        <MaskedInput
+          guide={true}
+          mask={[ ' ', /\d/, ' ', /\d/, ' ', ' ', /[ا-ی]/, ' ', ' ', /\d/, ' ', /\d/, ' ', /\d/, ' ']}
+          style={{ fontSize: '1.5rem' }}
+          showMask
+          autoFocus
+          dir=""
+        />
+        <FormHelperText id="outlined-weight-helper-text">پلاک ملی</FormHelperText>
+      </FormControl>
+    </Box>
   );
 }
